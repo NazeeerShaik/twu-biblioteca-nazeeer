2b88c866260504454241bae9e6773a88f5c40dde
NazeeerShaik <shaiknazeer6798@gmail.com>
2020-02-07 17:06:37 +0530

    (1.1)Add |Welcome message on app start|

---
+++ src/main/java/com/twu/biblioteca/BibliotecaApp.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class BibliotecaApp {
+
+    public static void main(String[] args) {
+        new BibliotecaApp().start();
+    }
+
+    public void start() {
+        String welcomeMessage = "Welcome to Biblioteca. Your one-stop-shop for great book titles in Bangalore!";
+        System.out.println(welcomeMessage);
+    }
+}
